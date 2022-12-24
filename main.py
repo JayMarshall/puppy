a428bcfc99e9eaf541f66813c77dcb2bb7a35d83
Alec M <alecm2011@gmail.com>
2022-12-23 23:15:05 -0500

    Update requirements, add User model, add make_user route

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from models import User
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.put("/user/{id}")
+def make_user(id: int, name: str, email: str):
+    newUser = User(id = id, name = name, email = email)
+    return(newUser.json())
